143bb79fb65352c0ff3f2c8ba89c21d40150e661
mark.nguyen1 <mark.nguyen1@vmodev.com>
2022-10-03 18:05:46 +0700

    update

--- src/middle/user.validation.ts
+++ src/middle/user.validation.ts
@@ -1,3 +1,12 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken';
 
-function userSignupRequest(req: Request, res: Response, next: NextFunction) {}
+function userSignupRequest(req: Request, res: Response, next: NextFunction) { }
+
+async function userAuth(req: Request, res: Response, next: NextFunction) {
+  const token: string = req.headers.authorization?.split(' ')?.[1] || req.headers.authorization?.split(' ')?.[0] || '';
+  const JWT_TOKEN = process.env.JWT_TOKEN || '';
+  const user = jwt.verify(token, JWT_TOKEN);
+  req.body.user = user;
+  next();
+}
