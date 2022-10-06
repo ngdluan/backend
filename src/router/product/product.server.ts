7289442e6dafdb1b5e1fb777cdb178a07ef87b08
mark.nguyen1 <mark.nguyen1@vmodev.com>
2022-10-06 17:22:39 +0700

    update

--- src/router/product/product.server.ts
+++ src/router/product/product.server.ts
@@ -7,12 +7,79 @@ import { errorMsg, httpCode, jwtType, successMsg } from "../../common/const";
 const prisma = new PrismaClient();
 export default {
   findById: async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.params, req.query)
     const id = req.params.id;
-    const product = prisma.product.findUniqueOrThrow({ where: { id } });
+    const product = await prisma.product.findUniqueOrThrow({
+      where: { id },
+      select: {
+        name: true,
+        description: true,
+        Price: {
+          select: {
+            id: true,
+            value: true,
+            Attr: {
+              select: {
+                key: true,
+                value: true
+              }
+            }
+          }
+        }
+      }
+    });
     res.status(200).json({ result: product });
   },
-  findMany: async (req: Request, res: Response, next: NextFunction) => {},
-  createProduct: async (req: Request, res: Response, next: NextFunction) => {},
-  updateProduct: async (req: Request, res: Response, next: NextFunction) => {},
-  deleteProduct: async (req: Request, res: Response, next: NextFunction) => {},
+  findMany: async (req: Request, res: Response, next: NextFunction) => {
+    const { page = 1, take = 50, search } = req.query;
+    const products = await prisma.product.findMany({
+      where: {},
+      select: {
+        name: true,
+        description: true,
+        Price: {
+          select: {
+            id: true,
+            value: true,
+            Attr: {
+              select: {
+                key: true,
+                value: true
+              }
+            }
+          }
+        }
+      },
+      skip: (Number(page) - 1) * Number(take),
+      take: Number(take),
+    })
+
+    res.status(httpCode.OK).json({
+      page: page,
+      results: products,
+    })
+
+  },
+  createProduct: async (req: Request, res: Response, next: NextFunction) => {
+    const product: Prisma.ProductCreateInput = req.body.product;
+
+    const productCreated = await prisma.product.create({ data: product })
+
+    res.status(httpCode.CREATED).json(productCreated)
+
+  },
+  updateProduct: async (req: Request, res: Response, next: NextFunction) => {
+    const product: Prisma.ProductUpdateInput = req.body.product;
+    const id = req.params.id;
+    const productUpdate = await prisma.product.update({
+      where: { id },
+      data: product
+    })
+    res.status(httpCode.OK).json({ result: productUpdate })
+  },
+  deleteProduct: async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const productDelete = await prisma.product.delete({ where: { id } })
+    res.status(httpCode.OK).json({ result: productDelete })
+  },
 };
