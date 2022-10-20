47a05c301622fcc572da008269d43aa8203d162a
mark.nguyen1 <mark.nguyen1@vmodev.com>
2022-10-20 11:35:30 +0700

    update schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { PostModule } from './post/post.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, ProductModule, PostModule],
+  imports: [UserModule, ProductModule, PostModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
