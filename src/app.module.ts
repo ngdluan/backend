c63de3db9156a82930bfccfa0549601e05a54aeb
mark.nguyen1 <mark.nguyen1@vmogroup.com>
2022-10-26 17:37:40 +0700

    update cart

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
 
 @Module({
-  imports: [UserModule, ProductModule, PostModule, AuthModule],
+  imports: [UserModule, ProductModule, PostModule, AuthModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
