5fab803d78247110532e43a2a982f03b56b6b51f
mark.nguyen1 <mark.nguyen1@vmodev.com>
2022-10-28 15:24:48 +0700

    udpate product service

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,47 +1,124 @@
+import { ProductVar } from './dto/base_product_dto';
 import { PrismaService } from './../prisma.service';
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { v4 } from 'uuid'
 import { Prisma } from "@prisma/client"
+import { validateHeaderValue } from 'http';
 
 @Injectable()
 export class ProductService {
   constructor(private prisma: PrismaService) { }
   async create(createProductDto: CreateProductDto) {
-    const { ProductVar, Tags, ...product } = createProductDto
-    let ProductAttr = []
-    let CheckUnique: { [key: string]: boolean } = {}
-
-    return await this.prisma.product.create({ data: createProductDto })
-
+    const { ProductVar, Tags, ...product } = { ...createProductDto, id: v4(), ProductAttr: { connect: [] } }
+    const createProductAttr: any[] = []
+    const CheckUnique: { [key: string]: boolean } = {}
+    const productVar: Prisma.ProductVarCreateInput[] = []
+    const productConnect = []
     for (let i = 0; i < ProductVar.length; i++) {
       if (ProductVar[i].ProductAttr) {
-        let productAttr: Prisma.ProductAttrWhereInput = { ...ProductVar[i].ProductAttr, keyValue: '' }
+        const ProductAttr = ProductVar[i].ProductAttr
         delete ProductVar[i].ProductAttr
-        productAttr.keyValue = `${productAttr.key}${productAttr.value}`.trim()
-        if (CheckUnique[productAttr.keyValue] !== true) {
-          CheckUnique[productAttr.keyValue] = true
-          ProductAttr.push(productAttr)
+
+        const ProductVarItem: Prisma.ProductVarCreateInput = {
+          ...ProductVar[i],
+          Product: { connect: { id: product.id } },
+          ProductAttr: { connect: [] }
         }
+
+        ProductVarItem.ProductAttr = { connect: [] }
+        const connect = []
+        for (let j = 0; j < ProductAttr.length; j++) {
+          const ProductAttrItem = ProductAttr[j]
+          connect.push({ key: ProductAttrItem.key, value: ProductAttrItem.value })
+          if (!CheckUnique[`${ProductAttrItem.key}${ProductAttrItem.value}`]) {
+            createProductAttr.push(this.prisma.productAttr.upsert({
+              where: { key_value: ProductAttrItem },
+              create: ProductAttrItem,
+              update: ProductAttrItem
+            }))
+            CheckUnique[`${ProductAttrItem.key}${ProductAttrItem.value}`] = true
+            productConnect.push({ key_value: ProductAttrItem })
+          }
+          connect.push({ key_value: ProductAttrItem })
+        }
+
+        ProductVarItem.ProductAttr.connect = productConnect;
+
+        productVar.push(ProductVarItem)
       }
     }
+    product.ProductAttr.connect = productConnect
+    const TagConnect = []
+    const createTag = []
+    for (let i = 0; i < Tags.length; i++) {
+      const tag = Tags[i]
+      createTag.push(this.prisma.tag.upsert({ where: { key_value: tag }, create: tag, update: tag }))
+      TagConnect.push({ key_value: tag })
+    }
 
-    const id = v4()
+    const tagConnect = [...Tags.map(tag => { key_value: tag })]
+
+    await this.prisma.$transaction([
+      ...createProductAttr,
+      ...createTag
+    ])
+    await this.prisma.$transaction([
+      this.prisma.product.create({
+        data: {
+          ...product, Tags: {
+            connect: TagConnect
+          }
+        }
+      }),
+      ...productVar.map(pv => this.prisma.productVar.create({ data: pv })),
+    ])
 
 
-    return 'This action adds a new product';
+    return { message: "Product successfully created" };
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findAll(where?: Prisma.ProductWhereInput, skip?: number, take?: number) {
+    const [result, count] = await this.prisma.$transaction([this.prisma.product.findMany({
+      where, select: {
+        name: true,
+        Tags: { select: { key: true, value: true } },
+        ProductVar: {
+          select: {
+            price: true,
+            public: true,
+            sku: true,
+            kiotId: true,
+            quantity: true,
+            ProductAttr: { select: { key: true, value: true } }
+          }
+        },
+      }, skip, take
+    }), this.prisma.product.count({ where })]);
+    return { count, result }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    return await this.prisma.product.findUnique({
+      where: { id }, select: {
+        name: true,
+        Tags: { select: { key: true, value: true } },
+        ProductVar: {
+          select: {
+            price: true,
+            public: true,
+            sku: true,
+            kiotId: true,
+            quantity: true,
+            ProductAttr: { select: { key: true, value: true } }
+          }
+        },
+      }
+    });
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  update(id: string, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
